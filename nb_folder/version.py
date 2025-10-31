9b5e94b8d347f29f1629af5c6aff117edabdd4c4
Allie Feras <allie.feras@snowflake.com>
2025-10-31 09:25:17 -0400

    gitignore

--- nb_folder/version.py
+++ nb_folder/version.py
@@ -4,23 +4,17 @@ import pickle
 from snowflake.ml.feature_store import FeatureView
 from snowflake.snowpark import DataFrame
 
-def version_featureview(feature_view:FeatureView, keys:list=[]) -> str:
+def version_featureview(feature_view:FeatureView) -> str:
 
-    """Computes an md5 hash of a feature view's query and the key/value of given metadata fields. 
-    This hash can be used as the version when registering the feature view.
-    By default, the function only includes the query in the feature view definition. 
-    Include additional keys that "matter" to the versioned definition in the keys argument."""
+    """Computes an md5 hash of a feature view's query and the key/value of metadata fields. 
+    This hash can be used as the version when registering the feature view."""
 
-    allow = [
-        'name','entities','timestamp_col','desc','feature_desc',
+    keys = [
+        'query', 'name','entities','timestamp_col','desc','feature_desc',
         'refresh_freq','database','schema','initialize','warehouse',
         'refresh_mode','refresh_mode_reason','owner','cluster_by'
     ]
-    bad_keys = list(set(keys) - set(allow))
-    if bad_keys:
-        raise KeyError(str(bad_keys) + " are not allowable keys for versioning")
-    to_hash = keys + ['query']
-    data = {k:str(getattr(feature_view, "_"+k)) for k in to_hash}
+    data = {k:str(getattr(feature_view, "_"+k)) for k in keys}
     return hashlib.md5(json.dumps(data, sort_keys=True).encode('utf-8')).hexdigest().upper()
 
 def version_data(df:DataFrame) -> str:
